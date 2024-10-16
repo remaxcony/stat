5e11308eb379d0cebc1b8da28990f3ba7b3b4faa
Remax Cony <kevin.espino@remax.es>
2024-10-16 14:10:48 +0100

    Commit

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "ENTRADA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.149101498783752,
+        "pitch": 0.05418542907808366,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2704073087320893,
+          "pitch": 0.041168297281039656,
+          "rotation": 0,
+          "target": "2-bao"
+        },
+        {
+          "yaw": -1.1337649023422465,
+          "pitch": 0.04163510558944061,
+          "rotation": 0,
+          "target": "3-dormitorio"
+        },
+        {
+          "yaw": -3.105215452168288,
+          "pitch": 0.038395555693597316,
+          "rotation": 0,
+          "target": "1-salon--cocina"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-salon--cocina",
+      "name": "SALON / COCINA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.629809203388703,
+        "pitch": -0.012323184081475702,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8520114424026275,
+          "pitch": 0.06424879242904247,
+          "rotation": 0,
+          "target": "0-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bao",
+      "name": "BAÑO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.0672651463360214,
+        "pitch": 0.09151944962867731,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.672486692950276,
+          "pitch": 0.02419189059227378,
+          "rotation": 0,
+          "target": "3-dormitorio"
+        },
+        {
+          "yaw": 1.1298153603486405,
+          "pitch": 0.052451077106152155,
+          "rotation": 0,
+          "target": "0-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dormitorio",
+      "name": "DORMITORIO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.831431632569439,
+        "pitch": 0.07896884374656743,
+        "fov": 1.292173873685646
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9480482926547111,
+          "pitch": 0.07141036914927668,
+          "rotation": 0,
+          "target": "2-bao"
+        },
+        {
+          "yaw": 0.6576385554618938,
+          "pitch": 0.0061684055082622535,
+          "rotation": 0,
+          "target": "0-entrada"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
